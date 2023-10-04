ca77907fdf3e0822d541cda7bfac2c9667d8fd64
Dmitrii Linkevich <bratokzs@yandex.ru>
2023-10-05 00:01:40 +0300

    install commanderjs, start make --help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('Проверка запуска исполняемого файла')
+import { program } from 'commander';
+
+const command = () => {
+    console.log('Проверка запуска исполняемого файла');
+  };
+
+  program
+  .version('0.0.1')
+  .action(command)
+  .description('Compares two configuration files and shows a difference.')
+
+  .parse(process.argv);
