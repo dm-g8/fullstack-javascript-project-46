8e779ef1c6f6f35bb2b58f031b8acc48b0608cce
Dmitrii Linkevich <bratokzs@yandex.ru>
2023-10-05 22:18:56 +0300

    step 1, finaly fix eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,12 +3,10 @@
 import { program } from 'commander';
 
 const command = () => {
-  console.log('Проверка запуска исполняемого файла');
 };
 
 program
   .version('0.0.1')
   .action(command)
   .description('Compares two configuration files and shows a difference.')
-
   .parse(process.argv);
